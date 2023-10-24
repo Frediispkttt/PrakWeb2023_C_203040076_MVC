3bc6ddc444a9c8796b2240c3e815f25af48a6fc0
“Frediispkttt” <fredisupangkat@gmail.com>
2023-10-24 20:34:26 +0700

    MVC

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+define('BASEURL', 'http://localhost/dashboard/PrakWeb2023_C_203040076_MVC');
+
+define('DEFAULT_CONTROLLER', 'Home');
+define('DEFAULT_METHOD', 'index');
+
+//DB
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
